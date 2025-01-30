32991424e1942d2d3552ab904d6abece2abe7bb8
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2025-01-30 16:50:34 +1100

    Fixed the CustomizedInput text field bugs

--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -1,26 +1,42 @@
-import { TextField } from '@mui/material';
+import React from 'react';
+import TextField from '@mui/material/TextField';
 
-type Props={
-    name: string;
-    type: string;
-    label: string;
+interface Props {
+  name: string;
+  type: string;
+  label: string;
 }
 
-const CustomizedInput = (props:Props) => {
+const CustomizedInput = (props: Props) => {
   return (
     <TextField
-        name={props.name}
-        label={props.label}
-        type={props.type}
-        slotProps={{
-        inputLabel: {
-            sx: { color: 'white' },
+      name={props.name}
+      label={props.label}
+      type={props.type}
+      slotProps={{ inputLabel: { sx: { color: 'white' } } }}
+      sx={{
+        width: 400,
+        mt: 2,
+        borderRadius: 2,
+        color: 'white',
+        fontSize: 12,
+        "& .MuiOutlinedInput-notchedOutline": {
+          borderColor: "white",
+          borderWidth: "2px",
         },
-        }}
-        sx={{ width: 400, borderRadius: 2, color: 'white', fontSize: 20, margin: "normal" }}
+        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+          borderColor: "white",
+        },
+        "& .MuiInputLabel-root": {
+          color: "white",
+        },
+        "& .Mui-focused .MuiInputLabel-root": {
+          color: "white",
+        },
+      }}
+      margin="normal"
     />
-  
-  )
-}
+  );
+};
 
-export default CustomizedInput
+export default CustomizedInput;
